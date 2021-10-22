9dcacfac7ddf161b05fb5d4900dbcfe63411f98d
stephanielambrighs <stephanielambrighs@hotmail.com>
2021-10-22 22:06:52 +0200

    added firstname, lastname and admin for user

--- IMDArchitecture.API/Controllers/DTOs/UserDTO.cs
+++ IMDArchitecture.API/Controllers/DTOs/UserDTO.cs
@@ -9,24 +9,31 @@ namespace IMDArchitecture.API.Controllers
     public class CreateUser
     {
         public int UserId { get; set; }
-        public string UserName { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
         public string Email { get; set; }
         public int Date_of_birth { get; set; }
-        public User ToUser() => new User { UserId = this.UserId, UserName = this.UserName, Email = this.Email, Date_of_birth = this.Date_of_birth };
+        public bool Administrator { get; set; }
+
+        public User ToUser() => new User { UserId = this.UserId, Firstname = this.Firstname, Lastname = this.Lastname, Email = this.Email, Date_of_birth = this.Date_of_birth, Administrator = this.Administrator };
     }
 
     public class ViewUser
     {
         public int UserId { get; set; }
-        public string UserName { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
         public string Email { get; set; }
         public int Date_of_birth { get; set; }
+        public bool Administrator { get; set; }
         public static ViewUser FromModel(User User) => new ViewUser
         {
             UserId = User.UserId,
-            UserName = User.UserName,
+            Firstname = User.Firstname,
+            Lastname = User.Lastname,
             Email = User.Email,
             Date_of_birth = User.Date_of_birth,
+            Administrator = User.Administrator,
         };
     }
 }
