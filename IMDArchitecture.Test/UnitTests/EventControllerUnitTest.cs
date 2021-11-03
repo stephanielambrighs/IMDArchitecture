1d19d5bb727e9cd6172da95c6076b1fb9edbd31a
stephanielambrighs <stephanielambrighs@hotmail.com>
2021-11-03 16:36:58 +0100

    added unittesting files

---
+++ IMDArchitecture.Test/UnitTests/EventControllerUnitTest.cs
@@ -0,0 +1,96 @@
+using System;
+using Xunit;
+using IMDArchitecture.API.Controllers;
+using Moq;
+using Microsoft.Extensions.Logging;
+using IMDArchitecture.API.Ports;
+using IMDArchitecture.API.Models;
+using IMDArchitecture.API.Domain;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IMDArchitecture.Test.UnitTests
+{
+    // Only partially tests the controller; the other endpoints are more of a reader's exercise. PR's welcome.
+    // Don't pull this on me with your own project ;-).
+    public class EventControllerUnitTest
+    {
+        // Mock the logger as STDOUT/STDIN is *** slow.
+        private Mock<ILogger<EventController>> _mockedLogger = new Mock<ILogger<EventController>>();
+        // Notice that we don't care what our database implementation looks like, since we "mock" (i.e. fake) it.
+        // We are testing the behaviour of our controller in this class, not of the database. 
+        private Mock<IDatabase> _mockedDatabase = new Mock<IDatabase>();
+
+        public EventControllerUnitTest()
+        {
+            _mockedDatabase.Reset();
+            _mockedLogger.Reset();
+        }
+
+        [Fact]
+        public async Task TestGetById_HappyPath()
+        {
+            // arrange
+            // this is our happy flow: we ask for the id of an existing application
+            var ourId = Guid.NewGuid();
+            var ourEvent = new Event { EventId = ourId, Name = "yes", Description = "New Event", Date = 2021, Participants = 2, Target_age = 18 };
+            // set up the mock so that when we call the 'GetMovieById' method we return a predefined task
+            // No database calls are happening here.
+            _mockedDatabase.Setup(x => x.GetEventById(ourId)).Returns(Task.FromResult(ourEvent));
+
+            // act
+            var controller = new EventController(_mockedLogger.Object, _mockedDatabase.Object);
+            var actualResult = await controller.GetEventById(ourId.ToString()) as OkObjectResult;
+
+            // assert
+            Assert.Equal(200, actualResult.StatusCode);
+            var viewModel = actualResult.Value as ViewEvent;
+            Assert.Equal(ourEvent.EventId.ToString(), viewModel.EventId);
+            Assert.Equal(ourEvent.Name, viewModel.Name);
+            Assert.Equal(ourEvent.Description, viewModel.Description);
+            Assert.Equal(ourEvent.Date, viewModel.Date);
+            Assert.Equal(ourEvent.Participants, viewModel.Participants);
+            Assert.Equal(ourEvent.Target_age, viewModel.Target_age);
+
+            _mockedLogger.VerifyAll();
+            _mockedDatabase.VerifyAll();
+        }
+
+        [Fact]
+        public async Task TestGetById_DoesntExist()
+        {
+            // arrange
+            var ourId = Guid.NewGuid();
+            var Event = new Event { EventId = ourId, Name = "yes", Description = "New Event", Date = 2021, Participants = 2, Target_age = 18 };
+            _mockedDatabase.Setup(x => x.GetEventById(ourId)).Returns(Task.FromResult(null as Event));
+
+            // act
+            var controller = new EventController(_mockedLogger.Object, _mockedDatabase.Object);
+
+            // assert
+            var result = await new EventController(_mockedLogger.Object, _mockedDatabase.Object).GetEventById(ourId.ToString());
+            Assert.IsType<NotFoundResult>(result);
+
+            _mockedLogger.VerifyAll();
+            _mockedDatabase.VerifyAll();
+        }
+
+        [Fact]
+        public async Task TestGetById_ErrorOnRetrievalAsync()
+        {
+            // arrange
+            var ourId = Guid.NewGuid();
+            var ourEvent = new Event { EventId = ourId, Name = "yes", Description = "New Event", Date = 2021, Participants = 2, Target_age = 18 };
+
+            _mockedDatabase.Setup(x => x.GetEventById(ourId)).ThrowsAsync(new Exception("Biebob"));
+
+            // act
+            var result = await new EventController(_mockedLogger.Object, _mockedDatabase.Object).GetEventById(ourId.ToString());
+
+            // assert
+            Assert.IsType<BadRequestObjectResult>(result);
+            _mockedLogger.VerifyAll();
+            _mockedDatabase.VerifyAll();
+        }
+    }
+}
---
+++ IMDArchitecture.Test/UnitTests/UserControllerUnitTest.cs
@@ -0,0 +1,97 @@
+using System;
+using Xunit;
+using IMDArchitecture.API.Controllers;
+using Moq;
+using Microsoft.Extensions.Logging;
+using IMDArchitecture.API.Ports;
+using IMDArchitecture.API.Models;
+using IMDArchitecture.API.Domain;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IMDArchitecture.Test.UnitTests
+{
+    // Only partially tests the controller; the other endpoints are more of a reader's exercise. PR's welcome.
+    // Don't pull this on me with your own project ;-).
+    public class UserControllerUnitTest
+    {
+        // Mock the logger as STDOUT/STDIN is *** slow.
+        private Mock<ILogger<UserController>> _mockedLogger = new Mock<ILogger<UserController>>();
+        // Notice that we don't care what our database implementation looks like, since we "mock" (i.e. fake) it.
+        // We are testing the behaviour of our controller in this class, not of the database. 
+        private Mock<IDatabase> _mockedDatabase = new Mock<IDatabase>();
+
+        public UserControllerUnitTest()
+        {
+            _mockedDatabase.Reset();
+            _mockedLogger.Reset();
+        }
+
+        [Fact]
+        public async Task TestGetById_HappyPath()
+        {
+            // arrange
+            // this is our happy flow: we ask for the id of an existing application
+            var ourId = Guid.NewGuid();
+            var ourUser = new User { UserId = ourId, Firstname = "Jurien", Lastname = "Rodi", Email = "jurien.rodi@hotmail.com", Date_of_birth = 20, Administrator = false };
+            // set up the mock so that when we call the 'GetMovieById' method we return a predefined task
+            // No database calls are happening here.
+            _mockedDatabase.Setup(x => x.GetUserById(ourId)).Returns(Task.FromResult(ourUser));
+
+            // act
+            var controller = new UserController(_mockedLogger.Object, _mockedDatabase.Object);
+            var actualResult = await controller.GetUserById(ourId.ToString()) as OkObjectResult;
+
+            // assert
+            Assert.Equal(200, actualResult.StatusCode);
+            var viewModel = actualResult.Value as ViewUser;
+            Assert.Equal(ourUser.UserId.ToString(), viewModel.UserId);
+            Assert.Equal(ourUser.Firstname, viewModel.Firstname);
+            Assert.Equal(ourUser.Lastname, viewModel.Lastname);
+            Assert.Equal(ourUser.Email, viewModel.Email);
+            Assert.Equal(ourUser.Date_of_birth, viewModel.Date_of_birth);
+            Assert.Equal(ourUser.Administrator, viewModel.Administrator);
+
+            _mockedLogger.VerifyAll();
+            _mockedDatabase.VerifyAll();
+        }
+
+        [Fact]
+        public async Task TestGetById_DoesntExist()
+        {
+            // arrange
+            var ourId = Guid.NewGuid();
+            var User = new User { UserId = ourId, Firstname = "Jurien", Lastname = "Rodi", Email = "jurien.rodi@hotmail.com", Date_of_birth = 20, Administrator = false };
+
+            _mockedDatabase.Setup(x => x.GetUserById(ourId)).Returns(Task.FromResult(null as User));
+
+            // act
+            var controller = new UserController(_mockedLogger.Object, _mockedDatabase.Object);
+
+            // assert
+            var result = await new UserController(_mockedLogger.Object, _mockedDatabase.Object).GetUserById(ourId.ToString());
+            Assert.IsType<NotFoundResult>(result);
+
+            _mockedLogger.VerifyAll();
+            _mockedDatabase.VerifyAll();
+        }
+
+        [Fact]
+        public async Task TestGetById_ErrorOnRetrievalAsync()
+        {
+            // arrange
+            var ourId = Guid.NewGuid();
+            var ourUser = new User { UserId = ourId, Firstname = "Jurien", Lastname = "Rodi", Email = "jurien.rodi@hotmail.com", Date_of_birth = 20, Administrator = false };
+
+            _mockedDatabase.Setup(x => x.GetUserById(ourId)).ThrowsAsync(new Exception("Jurien"));
+
+            // act
+            var result = await new UserController(_mockedLogger.Object, _mockedDatabase.Object).GetUserById(ourId.ToString());
+
+            // assert
+            Assert.IsType<BadRequestObjectResult>(result);
+            _mockedLogger.VerifyAll();
+            _mockedDatabase.VerifyAll();
+        }
+    }
+}
